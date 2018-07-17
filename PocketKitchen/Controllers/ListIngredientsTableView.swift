4d0217746c04dfc748fc2362597919fabc7b1007
Miguel Batilando <miguel.batilando@gmail.com>
2018-07-17 15:19:57 -0700

    implemented recipe search ui

---
+++ PocketKitchen/Controllers/ListIngredientsTableView.swift
@@ -0,0 +1,18 @@
+//
+//  ListIngredientsTableViewController.swift
+//  PocketKitchen
+//
+//  Created by Miguel Batilando on 7/16/18.
+//  Copyright © 2018 Simone Chan. All rights reserved.
+//
+
+import UIKit
+
+class ListIngredientsTableView: UITableView {
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    
+
+}
