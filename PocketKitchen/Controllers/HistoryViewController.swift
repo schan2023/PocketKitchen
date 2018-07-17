e1725114027e2ed86df0a61da57d1b6dd1597cb4
schan2023 <lamschan996@ufl.edu>
2018-07-16 17:03:31 -0700

    implemented recipe table view cell

---
+++ PocketKitchen/Controllers/HistoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HistoryViewController.swift
+//  PocketKitchen
+//
+//  Created by Simone Chan on 7/16/18.
+//  Copyright © 2018 Simone Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int ) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recipeTableViewCell", for: indexPath) as! RecipeTableViewCell
+        
+        cell.recipeTitleLabel.text = "Recipe 1"
+        cell.recipeDescriptionLabel.text = "This is the first recipe"
+        
+        return cell
+    }
+    
+}
---
+++ PocketKitchen/Views/RecipeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTableViewCell.swift
+//  PocketKitchen
+//
+//  Created by Simone Chan on 7/16/18.
+//  Copyright © 2018 Simone Chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var recipeDescriptionLabel: UILabel!
+    
+}
