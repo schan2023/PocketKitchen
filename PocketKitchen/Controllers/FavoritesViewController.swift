8234d17f7f760bf26f59e4348d2be3dd9b6cece8
Elaina <elainateramoto@gmail.com>
2018-07-18 10:32:56 -0700

    added display recipe UI design

---
+++ PocketKitchen/Controllers/FavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewController.swift
+//  PocketKitchen
+//
+//  Created by Elaina Teramoto on 7/18/18.
+//  Copyright © 2018 Simone Chan. All rights reserved.
+//
+
+import Foundation
